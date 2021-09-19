4f6e60a5d937b77b28050c91b20637d147c16121
Konark Lohat <konarklohat123456@gmail.com>
2021-09-19 23:44:28 +0530

    Type and editor added

--- database/migrations/2021_09_18_193005_create_posts_table.php
+++ database/migrations/2021_09_18_193005_create_posts_table.php
@@ -16,7 +16,7 @@ class CreatePostsTable extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->string("title");
-            $table->string("author");
+            $table->string("type");
             $table->string("content");
             $table->timestamps();
         });
